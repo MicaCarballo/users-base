e8d2a6c11f20816c12042ed758ba964c165fa168
MicaCarballo <micacarballo2@gmail.com>
2022-12-13 15:33:26 -0300

    delete authMiddleware function , fix secretOrKey

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,26 +4,24 @@ const {findUserById} = require('../users/users.controllers');
 const passport = require('passport');
 const jwtSecret = require('../../config').api.jwtSecret;
 
-const authMiddleware = () =>{
-    const options = {
-        jwtFromRequest : extractJwt.fromAuthHeaderWithScheme('jwt'),
-        secretKey: jwtSecret
-    }
-    passport.use(
-        new jwtStrategy(options, async(tokenDecoded,done)=>{
-            try{
-                const user = await findUserById(tokenDecoded.id)
-                if(!user){
-                    return done(null,false)
-                }
-                return done(null,tokenDecoded)
 
-            }catch(error){
-                return done(error,false)
-
-            }
-        })
-    )
-}
+const options = {
+    jwtFromRequest: extractJwt.fromAuthHeaderWithScheme("jwt"), //? Authorization JWT token
+    secretOrKey: jwtSecret,
+  };
+  passport.use(
+    new jwtStrategy(options, async (tokenDecoded, done) => {
+      //? done(error, tokenDecoded)
+      try {
+        const user = await findUserById(tokenDecoded.id);
+        if (!user) {
+          return done(null, false); //? No existe un error, pero tampoco existe el usuario
+        }
+        return done(null, tokenDecoded); //? No existe un error, pero si un usuario
+      } catch (error) {
+        return done(error, false); //? Si existe un error, pero no un usuario
+      }
+    })
+  );
 
 module.exports = passport;
